7c53c594b763b05707cfcf65c0b902d2fb7bd918
Braden Everson <bradeneverson@gmail.com>
2024-02-12 08:40:04 -0600

    Updated to Unda now everything's rocking

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,34 @@
 use std::error::Error;
-use triton_grow::network::{input::Input, network::Network};
+use unda::{core::{data::{input::Input, matrix::Matrix, matrix3d::Matrix3D}, network::Network, layer::{layers::{LayerTypes, InputTypes}, methods::{activations::Activations, errors::ErrorTypes}}}, util::{mnist::MnistEntry, categorical::to_categorical}};
+
+use rand::seq::SliceRandom;
 
 use breast_cancer::models::med_model::MedModel;
 
 fn main() -> Result<(), Box<dyn Error>>{
-    let mut models: Vec<MedModel> = MedModel::get_from_path("src/data/data.csv")?;
+    let mut models: Vec<(MedModel, f32)> = MedModel::get_from_path("src/data/data.csv")?;
+    let mut rng = rand::thread_rng();
+    models.shuffle(&mut rng);
 
-    let mut inputs: Vec<Vec<f32>> = vec![];
+    let mut inputs: Vec<&dyn Input> = vec![];
     let mut outputs: Vec<Vec<f32>> = vec![];
     models.iter_mut().for_each(|model| {
-        let inp_out = model.to_param();
-        outputs.push(vec![inp_out[0]]);
-        inputs.push(inp_out[1..inp_out.len()].to_vec());
+        outputs.push(vec![model.1]);
+        inputs.push(&model.0);
     });
-    
-    //let mut neural_net: Network = Network::new(vec![inputs[0].len(), inputs[0].len()*2, inputs[0].len()*3, inputs[0].len(), outputs[0].len()], triton_grow::network::activations::Activations::SIGMOID, 0.01);
-    //neural_net = neural_net.train_to_loss(inputs.clone(), outputs.clone(), None, 0.1, 10000, triton_grow::network::modes::Mode::Avg, 0.01, 0.0001, inputs[0].len(), inputs[0].len()*3);
-    //neural_net.train(&inputs, &outputs, 1000000, &triton_grow::network::modes::Mode::Avg);
 
-    //neural_net.save("breast_model2.json");
-    let mut neural_net: Network = Network::load("breast_model2.json");
-    neural_net = neural_net.train_to_loss(inputs.clone(), outputs.clone(), None, 0.1, 100000, triton_grow::network::modes::Mode::Avg, 0.01, 0.0001, inputs[0].len(), inputs[0].len()*3);
-    neural_net.save("breast_model3.json");
-    for i in 0..inputs.len(){
-        println!("Model predicted: {} \nActual: {}", neural_net.feed_forward(&inputs[i])[0], outputs[i][0]);
+    let mut network = Network::new(128);
+    network.set_input(InputTypes::DENSE(inputs[0].to_param().len()));
+    network.add_layer(LayerTypes::DENSE(16, Activations::RELU, 0.001));
+    network.add_layer(LayerTypes::DENSE(1, Activations::SIGMOID, 0.001));
+
+    network.compile();
+
+    network.fit(&inputs, &outputs, 1, ErrorTypes::CategoricalCrossEntropy);
+
+    for i in 0..10{
+        println!("Model output: {:?}\nActual: {:?}", network.predict(&inputs[i].to_param()), outputs[i]);
     }
+
     Ok(())
 }
--- src/models/med_model.rs
+++ src/models/med_model.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
-use triton_grow::{helper::csv_parser::CSVParse, network::input::Input};
+use unda::core::data::input::Input;
+use unda::util::csv_parser::CSVParse;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct MedModel{
@@ -38,16 +39,28 @@ pub struct MedModel{
 }
 
 impl MedModel{
-    pub fn get_from_path(path: &str) -> Result<Vec<MedModel>, Box<dyn std::error::Error>> {
+    pub fn get_from_path(path: &str) -> Result<Vec<(MedModel, f32)>, Box<dyn std::error::Error>> {
+        let mut mal = 0;
+        let mut norm = 0;
         let mut string_parsed: Vec<Vec<String>> = vec![];
         string_parsed.parse_elem(path)?;
         let mut float_parsed: Vec<Vec<f32>> = vec![];
         float_parsed.parse_elem(path)?;
-        let mut response: Vec<MedModel> = vec![];
+        let mut response: Vec<(MedModel, f32)> = vec![];
         for i in 0..string_parsed.len(){
+            if string_parsed[i][1].eq_ignore_ascii_case("m") {
+                mal+=1;
+            }else{
+                norm+=1;
+            }
             let new_model: MedModel = MedModel { id: float_parsed[i][0], diagnosis: string_parsed[i][1].clone(), radius_mean: float_parsed[i][2], texture_mean: float_parsed[i][3], perimeter_mean: float_parsed[i][4], area_mean: float_parsed[i][5], smoothness_mean: float_parsed[i][6], compactness_mean: float_parsed[i][7], concavity_mean: float_parsed[i][8], concave_points_mean: float_parsed[i][9], symmetry_mean: float_parsed[i][0], fractal_dimension_mean: float_parsed[i][0], radius_se: float_parsed[i][10], texture_se: float_parsed[i][11], perimeter_se: float_parsed[i][12], area_se: float_parsed[i][13], smoothness_se: float_parsed[i][14], compactness_se: float_parsed[i][15], concavity_se: float_parsed[i][0], concave_points_se: float_parsed[i][0], symmetry_se: float_parsed[i][16], fractal_dimension_se: float_parsed[i][17], radius_worst: float_parsed[i][18], texture_worst: float_parsed[i][0], perimeter_worst: float_parsed[i][0], area_worst: float_parsed[i][0], smoothness_worst: float_parsed[i][19], compactness_worst: float_parsed[i][20], concavity_worst: float_parsed[i][21], concave_points_worst: float_parsed[i][23], symmetry_worst: float_parsed[i][24], fractal_dimension_worst: float_parsed[i][25] };
-            response.push(new_model);
+            let mut malignent: f32 = 0.0;
+            if string_parsed[i][1].eq_ignore_ascii_case("m") {
+                malignent = 1.0;
+            }
+            response.push((new_model, malignent));
         }
+        println!("Malignant:{}\nNormal:{}", mal, norm);
         Ok(response)
     }
 }
@@ -60,8 +73,27 @@ impl Input for MedModel{
         }
         vec![malignent,
         self.smoothness_mean,
-        self.radius_mean/40.0,
-        self.texture_mean/40.0,
-        self.fractal_dimension_mean/100000.0]
+        self.area_mean.log(100.0),
+        self.concave_points_mean,
+        self.compactness_mean,
+        self.concavity_mean,
+        self.symmetry_mean.log(100.0),
+        self.radius_mean.log(100.0),
+        self.texture_mean.log(100.0),
+        self.fractal_dimension_mean.log(100.0),
+        self.perimeter_mean.log(100.0)
+        ]
+    }
+    fn to_param_2d(&self) -> Vec<Vec<f32>> {
+        vec![self.to_param()]
+    }
+    fn to_param_3d(&self) -> Vec<Vec<Vec<f32>>> {
+        vec![vec![self.to_param()]]
+    }
+    fn shape(&self) -> (usize, usize, usize) {
+        (self.to_param().len(), 1, 0)
+    }
+    fn to_box(&self) -> Box<dyn Input> {
+        Box::new(self.to_param())
     }
 }
